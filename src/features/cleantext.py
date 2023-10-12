b8be1bbead8045da864b7ccf5274a7ce7e06baca
trucndm <trucndmse172007@fpt.edu.vn>
2023-10-12 09:02:09 +0700

    refactor: refactor cleantext and checkdup file

--- src/features/cleantext.py
+++ src/features/cleantext.py
@@ -7,51 +7,56 @@ from unstructured.cleaners.core import remove_punctuation
 from unstructured.cleaners.core import replace_unicode_quotes
 
 
+# Define a class for text cleaning
 class TextClean:
     def __init__(self, text):
         self.text = text
 
+    # Method to clean HTML tags and extract text
     def clean_tag_html(self):
         elements = partition_html(text=self.text)
-        
         return elements[0].text
-    
+
+    # Method to clean text (removing bullets, extra whitespace, etc.)
     def clean_text(self):
         clean_text = clean(self.text, bullets=True, extra_whitespace=True, dashes=True, lowercase=True)
-
         return clean_text
-    
+
+    # Method to remove non-ASCII characters
     def clean_non_ascii_chars(self):
         clean_text = clean_non_ascii_chars(self.text)
-
         return clean_text
-    
+
+    # Method to clean postfix content
     def clean_postfix(self):
         clean_text = clean_postfix(self.text)
-
         return clean_text
-    
+
+    # Method to clean prefix content
     def clean_prefix(self):
         clean_text = clean_prefix(self.text)
-
         return clean_text
-    
+
+    # Method to remove trailing punctuation
     def clean_trailing_punctuation(self):
         clean_text = clean_trailing_punctuation(self.text)
-
         return clean_text
-    
+
+    # Method to remove all punctuation
     def remove_punctuation(self):
         clean_text = remove_punctuation(self.text)
-
         return clean_text
-    
+
+    # Method to replace Unicode quotes with ASCII quotes
     def replace_unicode_quotes(self):
         clean_text = replace_unicode_quotes(self.text)
-
         return clean_text
-    
+
 if __name__ == '__main__':
+    # Sample text containing HTML and emojis
     text = '"\n<html charset="utf-8"><p>Hello 😀</p></html>"'
+
+    # Create an instance of TextClean and clean the HTML tags
     Cleaner = TextClean(text)
-    print(Cleaner.clean_tag_html())
+    cleaned_html = Cleaner.clean_tag_html()
+    print(cleaned_html)
